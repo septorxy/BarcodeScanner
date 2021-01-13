4f0a78aa063a66778f8212def3b610e925a6c491
septorxy <liamgatt@gmail.com>
2021-01-13 15:07:52 +0100

    navigation updates

--- app/src/main/java/com/example/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/barcodescanner/MainActivity.kt
@@ -2,15 +2,11 @@ package com.example.barcodescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.example.barcodescanner.databinding.ActivityMainBinding
-import com.example.barcodescanner.databinding.FragmentTitleBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
